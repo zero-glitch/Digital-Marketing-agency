84a3c57c1148845f43791c17535df474ae7d538a
Akram <56227853+zero-glitch@users.noreply.github.com>
2022-07-04 03:38:25 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// Execute script after page load
+window.onload = function digital(){
+     //toggle button
+     let toggle = document.querySelector("#nav .toggle-btn")
+     let collapse = document.querySelector("#nav .collapse")
+
+     toggle.addEventListener('click'.onclick {
+        console.log(toggle);
+        
+     })
+}
+
+
+    
